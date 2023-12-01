f578532a7c79652c7677f656b6eb0d98d94e8a1c
subhamkrai <srai@redhat.com>
2023-12-01 18:02:29 +0530

    cmd: Add command to support osd-removal
    This commit add code to remove osd. Now, we first check
    if `osd-safe-to-destroy` if it not and user has passed
    `--force` we'll ask user ack to type `yes-i-really-want-to-destroy`.
    
    Signed-off-by: subhamkrai <srai@redhat.com>

---
+++ cmd/odf/ceph/purge_osd.go
@@ -0,0 +1,59 @@
+package rook
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/pkg/errors"
+	"github.com/red-hat-storage/odf-cli/cmd/odf/root"
+	"github.com/red-hat-storage/odf-cli/pkg/rook/osd"
+	"github.com/rook/kubectl-rook-ceph/pkg/logging"
+	"github.com/rook/kubectl-rook-ceph/pkg/mons"
+	"github.com/rook/kubectl-rook-ceph/pkg/rook"
+	"github.com/spf13/cobra"
+)
+
+var forceFlag = "force"
+
+var cephPurgeOsdCmd = &cobra.Command{
+	Use:     "purge-osd",
+	Short:   "Permanently remove an OSD from the cluster.",
+	Args:    cobra.ExactArgs(1),
+	Example: "odf ceph purge-osd <ID>",
+	PreRunE: validateOsdID,
+	Run: func(cmd *cobra.Command, args []string) {
+		ctx := cmd.Context()
+		clientsets := root.GetClientsets(ctx)
+		forceflagValue := "false"
+		osdID := args[0]
+
+		safeToDestroy, err := osd.SafeToDestroy(ctx, clientsets, root.OperatorNamespace, root.StorageClusterNamespace, osdID)
+		if !safeToDestroy {
+			var answer string
+			yesForceDestroyOSD := "yes-force-destroy-osd"
+			logging.Warning("Are you sure you want to purge osd.%s? The OSD is *not* safe to destroy. This may lead to data loss. If you are sure the OSD should be purged, enter '%s'", osdID, yesForceDestroyOSD)
+			fmt.Scanf("%s", &answer)
+
+			err := mons.PromptToContinueOrCancel("osd-purge", yesForceDestroyOSD, answer)
+			if err != nil {
+				logging.Fatal(err)
+			}
+
+			forceflagValue = "true"
+		} else if err != nil {
+			logging.Fatal(errors.Wrapf(err, "failed to check if osd.%s is safe to destroy", osdID))
+		}
+
+		rook.PurgeOsd(ctx, clientsets, root.OperatorNamespace, root.StorageClusterNamespace, osdID, forceflagValue)
+	},
+}
+
+func validateOsdID(cmd *cobra.Command, args []string) error {
+	osdID := args[0]
+	_, err := strconv.Atoi(osdID)
+	if err != nil {
+		return fmt.Errorf("Invalid ID %s, the OSD ID must be an integer. %v", osdID, err)
+	}
+
+	return nil
+}
