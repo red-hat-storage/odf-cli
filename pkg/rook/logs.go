ddb663e291bcfe522fd22c36098943863e595862
sp98 <sapillai@redhat.com>
2023-12-03 20:25:00 +0530

    cmd: validate ceph subsystems and support both log-level and memory level logs
    This PR takes care of:
    - supporting both log level and memory level changes in the command.
      For exampel:  odf set log-level osd ms 5/20
    - validates the ceph subsystems for logging
    
    Signed-off-by: sp98 <sapillai@redhat.com>

---
+++ pkg/rook/logs.go
@@ -0,0 +1,19 @@
+package rook
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/rook/kubectl-rook-ceph/pkg/exec"
+	"github.com/rook/kubectl-rook-ceph/pkg/k8sutil"
+	"github.com/rook/kubectl-rook-ceph/pkg/logging"
+)
+
+func SetCephLogLevel(ctx context.Context, clientsets *k8sutil.Clientsets, operatorNamespace, storageClusterNamespace string, component, subsystem, level string) {
+
+	cephArgs := []string{"config", "set", component, fmt.Sprintf("debug_%s", subsystem), level}
+
+	exec.RunCommandInOperatorPod(ctx, clientsets, "ceph", cephArgs, operatorNamespace, storageClusterNamespace, false, true)
+
+	logging.Info("successfully set the log levels for %q subsystem %q as %q", component, subsystem, level)
+}
